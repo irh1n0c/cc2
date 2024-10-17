a989efb0abb6f8ae7e0107d59d6881c03c2c1dc8
irh1n0c <lhuaynasi@gmail.com>
2024-10-16 21:11:47 -0500

    hola jose

---
+++ ejemplo.c
@@ -0,0 +1,20 @@
+#include "chess.h"
+#include "figures.h"
+
+
+void display(){
+    char** probado = malo;
+  char** probado = ok;
+  char joseok = probar;
+    char ** new = jose;
+  char** cuadradoB = whiteSquare;
+  char** cuadradoN = reverse(cuadradoB);
+
+  char** lineaT = repeatH(join(cuadradoB, cuadradoN), 4);
+  char** lineaInversa = repeatH(join(cuadradoN, cuadradoB), 4);
+  interpreter(lineaInversa);
+  interpreter(queen);
+  
+  
+}
+
