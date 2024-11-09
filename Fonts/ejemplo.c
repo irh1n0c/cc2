573fdc243e3b4e050fe981df16d7acc5d0eef034
irh1n0c <lhuaynasi@gmail.com>
2024-11-08 21:46:11 -0500

    update

---
+++ Fonts/ejemplo.c
@@ -0,0 +1,11 @@
+#include "chess.h"
+#include "figures.h"
+#include "gc.h"
+
+void display(){
+  char** blackSquare = reverse(whiteSquare);
+  interpreter(blackSquare);
+  probado por jose;
+  template;
+  garbageCollector();
+}
